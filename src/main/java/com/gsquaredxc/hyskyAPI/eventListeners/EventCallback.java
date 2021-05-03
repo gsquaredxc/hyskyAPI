fc38582c5a204fac76c91a216caf7d18eeb833e5
gsquaredxc <ggarrison5@gmail.com>
2021-05-02 21:13:49 -0500

    Refactor Method into MethodHandle, thanks DJtheRedstoner!

--- src/main/java/com/gsquaredxc/hyskyAPI/eventListeners/EventCallback.java
+++ src/main/java/com/gsquaredxc/hyskyAPI/eventListeners/EventCallback.java
@@ -1,36 +1,35 @@
 package com.gsquaredxc.hyskyAPI.eventListeners;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.invoke.MethodHandle;
 
 public class EventCallback {
     protected String name = "";
-    protected Method func;
+    protected MethodHandle func;
 
-    public EventCallback(final Method func) {
+    public EventCallback(final MethodHandle func) {
         this.func = func;
     }
-    public EventCallback(final Method func, final String name) {
+    public EventCallback(final MethodHandle func, final String name) {
         this.func = func;
         this.name = name;
     }
 
     public void callBackToFunction(final Event e){
         try {
-            func.invoke(func.getDeclaringClass(),e);
-        } catch (final IllegalAccessException | InvocationTargetException illegalAccessException) {
+            func.invoke(e);
+        } catch (final Throwable illegalAccessException) {
             illegalAccessException.printStackTrace();
         }
     }
 
     public boolean callBackToFunctionCancellable(final Event e){
         try {
-            return (boolean) func.invoke(func.getDeclaringClass(),e);
-        } catch (final IllegalAccessException | InvocationTargetException illegalAccessException) {
-            illegalAccessException.printStackTrace();
+            return (boolean) func.invoke(e);
         } catch (final NullPointerException error){
             error.printStackTrace();
             System.err.println("This error may be caused by the listener function returning void instead of boolean. ");
+        } catch (final Throwable illegalAccessException) {
+            illegalAccessException.printStackTrace();
         }
         return false;
     }
--- src/main/java/com/gsquaredxc/hyskyAPI/eventListeners/EventRegister.java
+++ src/main/java/com/gsquaredxc/hyskyAPI/eventListeners/EventRegister.java
@@ -3,15 +3,23 @@ package com.gsquaredxc.hyskyAPI.eventListeners;
 import com.gsquaredxc.hyskyAPI.PublicListeners;
 import com.gsquaredxc.hyskyAPI.annotations.EventListener;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 
 public class EventRegister {
     public static void register(final Class<?> c){
+        final MethodHandles.Lookup lookup = MethodHandles.lookup();
         for (final Method m: c.getDeclaredMethods()){
             if (m.isAnnotationPresent(EventListener.class) && m.getParameterCount() == 1){
                 final EventListener eventl = m.getAnnotation(EventListener.class);
-                final com.gsquaredxc.hyskyAPI.eventListeners.EventListener eventListener = PublicListeners.listenerHashMap.get(m.getParameterTypes()[0]);
-                eventListener.register(new EventCallback(m, eventl.id()));
+                try {
+                    final MethodHandle handledMethod = lookup.unreflect(m);
+                    final com.gsquaredxc.hyskyAPI.eventListeners.EventListener eventListener = PublicListeners.listenerHashMap.get(m.getParameterTypes()[0]);
+                    eventListener.register(new EventCallback(handledMethod, eventl.id()));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
