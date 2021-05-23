76bb0a2cb2a4dbc4b833e2eba9e7fa85b1fec11f
gsquaredxc <ggarrison5@gmail.com>
2021-05-23 17:53:33 -0500

    Optimize InSkyblock.java

--- src/main/java/com/gsquaredxc/hyskyAPI/StateRegister/InSkyblock.java
+++ src/main/java/com/gsquaredxc/hyskyAPI/StateRegister/InSkyblock.java
@@ -4,6 +4,8 @@ import com.gsquaredxc.hyskyAPI.annotations.EventListener;
 import com.gsquaredxc.hyskyAPI.events.custom.ServerTypeKnownEvent;
 import com.gsquaredxc.hyskyAPI.events.custom.SkyblockDisconnectEvent;
 
+import static com.gsquaredxc.hyskyAPI.PrivateListeners.ServerTypeKnownListenerO;
+
 /**
  * InSkyblock is a state register for if the player is in Skyblock or not.
  * {@inheritDoc}
@@ -15,9 +17,11 @@ public class InSkyblock extends StateRegister{
     @EventListener(id="INTERNALInSkyblockactive")
     public void serverTypeKnown(final ServerTypeKnownEvent e){
         onPositiveState();
+        ServerTypeKnownListenerO.deregister("INTERNALInSkyblockactive");
     }
     @EventListener(id="INTERNALInSkyblockdisabled")
     public void disconnected(final SkyblockDisconnectEvent e){
         onNegativeState();
+        ServerTypeKnownListenerO.reregister("INTERNALInSkyblockactive");
     }
 }
